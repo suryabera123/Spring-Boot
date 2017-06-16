d9f7426dd0f4199f7c59d600f5d13b86e1a2c42c
suryabera1 <Password@123>
2017-06-16 18:25:27 -0400

    Final version of spring mvc, jdbc and embedded tomcat.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -3,12 +3,19 @@ package hello;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.inject.Inject;
+import java.util.List;
+
 @RestController
 public class HelloController {
 
+    @Inject
+    private Test1Dao test1Dao;
+
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!++++++++++";
+        List<Test1> list = test1Dao.getTestData();
+        return "Greetings from Spring Boot!++++++++++test";
     }
 
 }
